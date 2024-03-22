a181d14bce8011c401629006e7f5bc642c179fca
Nikhil Gupta <nikhilg7404@gmail.com>
2024-03-22 22:06:05 +0530

    some updates in input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { forwardRef } from "react";
+import { ForwardedRef } from "react";
 
-const Input = ({ label, type, istextArea, ...props }) => {
+const Input = forwardRef(({ label, type, istextArea, ...props }, ref) => {
   return (
     <div className="mt-5">
       <label className="uppercase font-medium">{label}</label>
-      {istextArea ? <textarea className="block w-full bg-orange-200 bg-opacity-40 focus:outline-none focus:border-blue-300 focus:border-b-4" cols="59" rows="3"></textarea> : <input className="block h-12 w-full bg-orange-200 bg-opacity-40 focus:outline-none focus:border-blue-300 focus:border-b-4" type={type}></input>}
+      {istextArea ? (
+        <textarea
+          ref={ref}
+          className="block w-full bg-orange-200 bg-opacity-40 focus:outline-none focus:border-blue-300 focus:border-b-4"
+          cols="59"
+          rows="3"
+        ></textarea>
+      ) : (
+        <input
+          ref={ref}
+          className="block h-12 w-full bg-orange-200 bg-opacity-40 focus:outline-none focus:border-blue-300 focus:border-b-4"
+          type={type}
+        ></input>
+      )}
     </div>
   );
-};
+});
 
 export default Input;
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import Input from "./Input";
+import { useRef } from "react";
 
 const NewProject = ({onCancel}) => {
+  const title = useRef()
+  const description = useRef()
+  const dueDate = useRef()
+
+  function handleSave()
+  {
+    const projectTitle = title.current.value
+    const projectDescription = description.current.value
+    const projectDueDAte = dueDate.current.value
+    return(<div>
+      <h2 className="text-2xl my-7 font-medium text-white">
+        {projectTitle}
+      </h2>
+      <p>Due for: {projectDueDAte}</p>
+    </div>)
+
+  }
+
+
   return (
     <div className="mt-4 w-[40rem]">
       <div className="flex flex-row-reverse gap-5 ml-72">
-        <button className="rounded-xl text-white text-lg h-10 w-16 bg-black">Save</button>
+        <button onClick={handleSave} className="rounded-xl text-white text-lg h-10 w-16 bg-black">Save</button>
         <button className=" text-black text-lg h-10 w-16" onClick={onCancel}>Cancel</button>
       </div>
-      <Input label={"Title"} type={"text"}></Input>
-      <Input label={"description"} istextArea={true}></Input>
-      <Input label={"due date"} type={"date"}></Input>
+      <Input ref={title} label={"Title"} type={"text"}></Input>
+      <Input ref={description} label={"description"} istextArea={true}></Input>
+      <Input ref={dueDate} label={"due date"} type={"date"}></Input>
     </div>
   );
 };
